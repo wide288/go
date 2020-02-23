ffb3ddff0dc5072d1b9fa78ec39339ae84a23055
wide288 <wide288@163.com>
2020-02-23 22:10:20 +0800

    datetime test unit

--- datetime/datetime_test.go
+++ datetime/datetime_test.go
@@ -1,6 +1,9 @@
 package datetime
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestTimeUnixToStr(t *testing.T) {
 	type args struct {
@@ -24,6 +27,48 @@ func TestTimeUnixToStr(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := TimeUnixToStr(tt.args.unix); got != tt.want {
 				t.Errorf("TimeUnixToStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+/**
+cur month time : 2020-2-23 22:09:30
+return:
+	got: 2020-02-01 12:00:00
+	got1: 2020-03-01 12:00:00
+ */
+func TestGetLastMonth(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int64
+		want1 int64
+	}{
+		// TODO: Add test cases.
+		{
+			name:  "1 month",
+			args:  args{
+				num: 1,
+			},
+			want:  0,
+			want1: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := GetLastMonth(tt.args.num)
+			fmt.Println("got:", TimeUnixToStr(got))
+			fmt.Println("got1:", TimeUnixToStr(got1))
+
+			if got != tt.want {
+				t.Errorf("GetLastMonth() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("GetLastMonth() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
